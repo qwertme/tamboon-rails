3540b4812cfa222d88150d4dbb7d554ef35c4632
Konstantin T <mrootooz@gmail.com>
2017-09-28 16:29:28 +0300

    Fix the race condition in the balance amount column. - ready

--- rails/app/models/charity.rb
+++ rails/app/models/charity.rb
@@ -2,7 +2,8 @@ class Charity < ActiveRecord::Base
   validates :name, presence: true
 
   def credit_amount(amount)
-    new_total = total + amount
+    model = Charity.find(self.id)
+    new_total = model.total + amount
     update_attributes({:total => new_total})
   end
 end
